90fea450d597f3b792452370395daa1161f7684e
josve <josve@DESKTOP-LPR10BG>
2018-11-26 19:36:06 -0300

    primera fuente desde eclipse

---
+++ src/main/java/com/calidad/ws/rest/vo/VOEjercicio.java
@@ -0,0 +1,22 @@
+package com.calidad.ws.rest.vo;
+
+public class VOEjercicio {
+
+	private String pregunta;
+	private String fuente;
+	
+	
+	public String getPregunta() {
+		return pregunta;
+	}
+	public void setPregunta(String pregunta) {
+		this.pregunta = pregunta;
+	}
+	public String getFuente() {
+		return fuente;
+	}
+	public void setFuente(String fuente) {
+		this.fuente = fuente;
+	}	
+	
+}
