9de1a1b56097ee4b9d9329d9f5db4aca3bd7df5d
josveg1987 <josveg1987@gmail.com>
2018-11-29 21:03:38 -0300

    se agrega back de validacion

--- src/main/java/com/calidad/ws/rest/vo/VOValidaEjercicio.java
+++ src/main/java/com/calidad/ws/rest/vo/VOValidaEjercicio.java
@@ -3,6 +3,7 @@ package com.calidad.ws.rest.vo;
 public class VOValidaEjercicio {
 	
 	private int id;
+	private String respuesta;
 
 	public int getId() {
 		return id;
@@ -10,6 +11,15 @@ public class VOValidaEjercicio {
 
 	public void setId(int id) {
 		this.id = id;
-	}	
+	}
+
+	public String getRespuesta() {
+		return respuesta;
+	}
+
+	public void setRespuesta(String respuesta) {
+		this.respuesta = respuesta;
+	}
+	
 
 }
