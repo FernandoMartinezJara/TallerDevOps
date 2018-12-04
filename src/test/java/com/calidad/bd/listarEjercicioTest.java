6f28f0f0f224ff15a4ef7ae814198fe802887ccb
josveg1987 <josveg1987@gmail.com>
2018-12-03 21:13:40 -0300

    junit

---
+++ src/test/java/com/calidad/bd/listarEjercicioTest.java
@@ -0,0 +1,39 @@
+package com.calidad.bd;
+
+import static org.junit.Assert.*;
+import org.junit.Assert;
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class listarEjercicioTest {
+
+	@Test
+	public void testListarEjercicio() {
+		listarEjercicio le = null;
+		try {
+			le = new listarEjercicio(0);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		le.getEjercicio().getId();
+		Assert.assertNotNull(le.getEjercicio());	
+	}
+
+	@Test
+	public void testGetEjercicio() {
+		listarEjercicio le = null;
+		try {
+			le = new listarEjercicio(0);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		le.getEjercicio().getId();
+		Assert.assertNotNull(le.getEjercicio());	
+	}
+
+}
